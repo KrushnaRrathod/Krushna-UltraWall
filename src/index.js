2e2794f80dc65e896848f1b60bb2a2df7e51356f
Krushna Rathod <rathodkrushna2405@gmail.com>
2024-07-14 02:53:08 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,180 @@
+const data = [
+    {
+        src: "/gradient.png",
+        title: 'GRADIENT'
+    },
+    {
+        src: "/nature.png",
+        title: 'NATURE'
+    },
+    
+    {
+        src: "/marvel.png",
+        title: 'MARVEL'
+    },
+    {
+        src: "/warp.jpg",
+        title: 'WARP'
+    },
+    {
+        src: "/waves.jpg",
+        title: 'WAVES'
+    },
+    {
+        src: "/dark.png",
+        title: "DARK"
+    },
+    {
+        src: "/art.png",
+        title: 'ART'
+    },
+    {
+        src: "/mountain.png",
+        title: "MOUNTAIN"
+    },
+    {
+        src: "/sci-fi.png",
+        title: "SCI-FI"
+    },
+    {
+        src: "/oil-rig.png",
+        title: "OIL-RIG"
+    }
+]
+
+const gradient = [
+    {
+        url: "/gradient/g1.jpg"
+    },
+    {
+        url: "/gradient/g2.jpg"
+    },
+    {
+        url: "/gradient/g3.jpg"
+    },
+    {
+        url: "/gradient/g4.jpg"
+    },
+    {
+        url: "/gradient/g5.jpg"
+    },
+    {
+        url: "/gradient/g6.jpg"
+    },
+]
+
+const marvel = [
+    {
+        url: "/marvel/m1.jpg"
+    },
+    {
+        url: "/marvel/m2.jpg"
+    },
+    {
+        url: "/marvel/m3.jpg"
+    },
+    {
+        url: "/marvel/m4.jpg"
+    },
+    {
+        url: "/marvel/m5.jpg"
+    },
+]
+
+const nature = [
+    {
+        url: "/nature/n1.jpg"
+    },
+    {
+        url: "/nature/n2.jpg"
+    },
+    {
+        url: "/nature/n3.png"
+    },
+    {
+        url: "/nature/n4.jpg"
+    },
+    {
+        url: "/nature/n5.jpg"
+    },
+    {
+        url: "/nature/n6.jpg"
+    },
+]
+
+const art = [
+    {
+        url: "/art/a1.png"
+    },
+    {
+        url: "/art/a2.jpg"
+    },
+    {
+        url: "/art/a1.jpg"
+    },
+    {
+        url: "/art/a1.jpg"
+    },
+]
+
+const waves = [
+    {
+        url: '/waves/w1.jpg'
+    },
+    {
+        url: '/waves/w2.jpg'
+    },
+    {
+        url: '/waves/w3.jpg'
+    },
+]
+
+const warp = [
+    {
+        url: '/warp/Warp-1.jpg'
+    },
+    {
+        url: '/warp/Warp-5.jpg'
+    },
+]
+
+const mountain = [
+    {
+        url: "/mountain/m1.jpg"
+    },
+    {
+        url: "/mountain/m2.jpg"
+    },
+    {
+        url: "/mountain/m3.jpg"
+    },
+]
+
+const scifi = [
+    {
+        url: "/sci-fi/sf-1.jpg"
+    },
+    {
+        url: "/sci-fi/sf-2.jpg"
+    },
+]
+
+const dark = [
+    {
+        url: "/dark/d1.jpg"
+    },
+    {
+        url: "/dark/d2.jpg"
+    }
+]
+
+const oilrig = [
+    {
+        url: "/oil-rig/oil1.jpg"
+    },
+    {
+        url: "/oil-rig/oil2.jpg"
+    }
+]
+
+export {data, gradient, marvel, nature, art, waves, warp, mountain, scifi, dark, oilrig};
